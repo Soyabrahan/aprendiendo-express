e19010ac972c8da4b117481e5f4bba23493b4ff1
Abrahan <abrahanramirez2410@gmail.com>
2025-05-09 13:00:39 -0400

    empezando express

---
+++ cursos.js
@@ -0,0 +1,42 @@
+let infoCursos = {
+    "programacion" : [
+        {
+            id : 1,
+            titulo : "Aprende Python",
+            lenguaje : "Python",
+            vistas : 15000,
+            nivel : "basico"
+        },
+        {
+            id : 2,
+            titulo : "Aprende Javascript",
+            lenguaje : "Javascript",
+            vistas : 102223,
+            nivel : "basico"
+        },
+        {
+            id : 3,
+            titulo : "Aprende Python Intermedio",
+            lenguaje :"Python",
+            vistas : 125400,
+            nivel : "intermedio"
+        }
+    ],
+    "matematicas" : [ {
+        id : 1,
+        titulo : "Aprende Calculo",
+        tema : "Calculo",
+        vistas : 150000,
+        nivel : "basico"
+    },
+    {
+        id : 2,
+        titulo : "Aprende Algebra",
+        tema : "Algebra",
+        vistas : 10223,
+        nivel : "basico"
+    }
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
